6afa955991a1d05b32a536d6dc2fe1f166674a0b
Sayyed Mohammad Miri <smmmiri@outlook.com>
2024-01-28 10:31:13 +0330

    Created three test methods in Northwind.UnitTests project

--- Northwind.UnitTests/EntityModelTests.cs
+++ Northwind.UnitTests/EntityModelTests.cs
@@ -1,11 +1,34 @@
-namespace Northwind.UnitTests
-{
-    public class EntityModelTests
-    {
-        [Fact]
-        public void Test1()
-        {
+namespace Northwind.EntityModels;
 
-        }
+public class EntityModelTests
+{
+    [Fact]
+    public void DatabaseConnectTest()
+    {
+        using NorthwindContext db = new();
+        Assert.True(db.Database.CanConnect());
+    }
+
+    [Fact]
+    public void CategoryCountTest()
+    {
+        using NorthwindContext db = new();
+
+        var expected = 8;
+        var actual = db.Categories.Count();
+
+        Assert.Equal(expected, actual);
+    }
+
+    [Fact]
+    public void ProductId1IsChaiTest()
+    {
+        using NorthwindContext db = new();
+
+        var expected = "Chai";
+        var product = db.Products.Find(1);
+        var actual = product?.ProductName ?? string.Empty;
+
+        Assert.Equal(expected, actual);
     }
 }
