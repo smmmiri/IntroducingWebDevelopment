811c9d541e32b33b4b2a6c1fdd8f6697003cd934
Sayyed Mohammad Miri <smmmiri@outlook.com>
2024-01-28 16:02:05 +0330

    Modified program

--- Northwind.Web/Program.cs
+++ Northwind.Web/Program.cs
@@ -3,4 +3,6 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.Run();
+// Start the web server, host the website, and wait for requests.
+app.Run(); // This is a thread-blocking call.
+Console.WriteLine("This executes after the web server has stopped!");
