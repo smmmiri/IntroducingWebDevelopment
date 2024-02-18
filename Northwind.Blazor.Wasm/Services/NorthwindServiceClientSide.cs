d1fc0a8bb2cd8a730f5372296ff6c6596a20b2e0
Sayyed Mohammad Miri <smmmiri@outlook.com>
2024-02-18 11:15:35 +0330

    Added Blazor.Wasm project that run blazor in the client side

---
+++ Northwind.Blazor.Wasm/Services/NorthwindServiceClientSide.cs
@@ -0,0 +1,75 @@
+﻿using Northwind.EntityModels; // To use Customer.
+using System.Net.Http.Json; // To use GetFromJsonAsync.
+
+namespace Northwind.Blazor.Services;
+
+public class NorthwindServiceClientSide : INorthwindService
+{
+	private readonly IHttpClientFactory _clientFactory;
+
+	public NorthwindServiceClientSide(IHttpClientFactory httpClientFactory)
+	{
+		_clientFactory = httpClientFactory;
+	}
+
+	public Task<List<Customer>> GetCustomersAsync()
+	{
+		HttpClient client = _clientFactory.CreateClient(
+		  name: "Northwind.WebApi");
+
+		return client.GetFromJsonAsync
+		  <List<Customer>>("api/customers")!;
+	}
+
+	public Task<List<Customer>> GetCustomersAsync(string country)
+	{
+		HttpClient client = _clientFactory.CreateClient(
+		  name: "Northwind.WebApi");
+
+		return client.GetFromJsonAsync
+		  <List<Customer>>($"api/customers/in/{country}")!;
+	}
+
+	public Task<Customer?> GetCustomerAsync(string id)
+	{
+		HttpClient client = _clientFactory.CreateClient(
+		  name: "Northwind.WebApi");
+
+		return client.GetFromJsonAsync
+		  <Customer>($"api/customers/{id}");
+	}
+
+	public async Task<Customer>
+	  CreateCustomerAsync(Customer c)
+	{
+		HttpClient client = _clientFactory.CreateClient(
+		  name: "Northwind.WebApi");
+
+		HttpResponseMessage response = await
+		  client.PostAsJsonAsync("api/customers", c);
+
+		return (await response.Content
+		  .ReadFromJsonAsync<Customer>())!;
+	}
+
+	public async Task<Customer> UpdateCustomerAsync(Customer c)
+	{
+		HttpClient client = _clientFactory.CreateClient(
+		  name: "Northwind.WebApi");
+
+		HttpResponseMessage response = await
+		  client.PutAsJsonAsync("api/customers", c);
+
+		return (await response.Content
+		  .ReadFromJsonAsync<Customer>())!;
+	}
+
+	public async Task DeleteCustomerAsync(string id)
+	{
+		HttpClient client = _clientFactory.CreateClient(
+		  name: "Northwind.WebApi");
+
+		HttpResponseMessage response = await
+		  client.DeleteAsync($"api/customers/{id}");
+	}
+}
